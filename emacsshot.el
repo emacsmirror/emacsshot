cafc6bbb3cb12e106bf1ac67216c5174dc5fecfa
Marco Wahl <marcowahlsoft@gmail.com>
2014-04-29 21:33:21 +0200

    v 0.1: message after shot + better text in the program

--- emacsshot.el
+++ emacsshot.el
@@ -1,24 +1,27 @@
-;; emacsshot.el -- Snapshot an Emacs-frame from within emacs.
+;;; emacsshot.el -- Snapshot an Emacs-frame from within emacs
+
 ;; Copyright 2014 Marco Wahl
-;;
+
+;; Keywords: screenshot
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;;
+
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
-;;
-;; Version: 0.0
+
+;; Version: 0.1
 ;; Created: 2014-01-26
 ;; Contact: marcowahlsoft()gmail.com
-;;
-;;
+
+
 ;; Activation:
 ;;
 ;; Activate this program by loading it into Emacs and evaluate it with
@@ -30,7 +33,7 @@
 ;; (add-to-list 'load-path "/...path to this program...")
 ;; (require 'emacsshot)
 ;;
-;; to your emacs-configuration-file, typically called '.emacs'.
+;; to your emacs-configuration-file.
 ;;
 ;; Optional: Set a filename for the snapshot-image.  This is the value
 ;; of variable `eshot-snap-frame-filename'.  You can customize it via
@@ -44,8 +47,8 @@
 ;;
 ;; M-x eshot-snap-frame
 ;;
-;; It might be a good idea to activate the function by a key.
-;; You could define the print key to trigger the function-call with
+;; It might be a good idea to set the function to a key in order to
+;; avoid Heisenshots.  E.g. the print-key could trigger the shot
 ;;
 ;; M-x eval-expression (global-set-key (kbd "<print>") 'eshot-snap-frame)
 ;;
@@ -81,7 +84,11 @@ snapped."
   (call-process
    "convert" nil (get-buffer-create "*convert-output*") nil
    (format "x:%s" (frame-parameter nil 'outer-window-id))
-   (expand-file-name eshot-snap-frame-filename)))
+   (expand-file-name eshot-snap-frame-filename))
+  (message
+   (concat
+    "Written file "
+    (expand-file-name eshot-snap-frame-filename))))
 
 
 (provide 'emacsshot)
